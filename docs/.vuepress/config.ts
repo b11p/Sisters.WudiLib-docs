8c21fd025eb524487a161abe2111b16ce9e4bbeb
bleatingsheep <bleatingsheep@live.cn>
2021-05-06 03:37:17 +0900

    Update suggestions, add roadmap.

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -9,7 +9,7 @@ export default defineUserConfig<DefaultThemeOptions>({
     themeConfig: {
         navbar: [
             { text: '主页', link: '/' },
-            { text: '未来', children: ['/lantu.md', 'gei-sdk-kaifazhedejianyi.md'] },
+            { text: '未来', children: ['luxiantu.md', '/lantu.md', 'gei-sdk-kaifazhedejianyi.md'] },
         ],
         repo: 'int-and-his-friends/Sisters.WudiLib',
         editLink: true,
