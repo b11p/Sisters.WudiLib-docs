427e2d577b1f21fa2a3d6f61fd6751e336ad0641
b11p <b11p@users.noreply.github.com>
2022-05-10 14:12:15 +0000

    deploy: 9fa6136738c30b5b70307bec75067fd315e2b2a4

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,25 @@
 if (!self.define) {
-    let s, e = {};
-    const l = (l, i) => (l = new URL(l + ".js", i).href, e[l] || new Promise((e => { if ("document" in self) { const s = document.createElement("script"); s.src = l, s.onload = e, document.head.appendChild(s) } else s = l, importScripts(l), e() })).then((() => { let s = e[l]; if (!s) throw new Error(`Module ${l} didn’t register its module`); return s }))); self.define = (i, n) => { const a = s || ("document" in self ? document.currentScript.src : "") || location.href; if (e[a]) return; let t = {}; const u = s => l(s, a), r = { module: { uri: a }, exports: t, require: u }; e[a] = Promise.all(i.map((s => r[s] || u(s)))).then((s => (n(...s), t))) }
+    navigator.serviceWorker && navigator.serviceWorker.getRegistrations().then(function (registrations) {
+        for (let registration of registrations) {
+            registration.unregister();
+            console.log('Unregistered.');
+        }
+        console.log('All unregistered.');
+    }).catch(function (err) {
+        console.log('Service Worker registration failed: ', err);
+    });
+    // let s, e = {};
+    // const l = (l, i) => (l = new URL(l + ".js", i).href, e[l] || new Promise((e => { if ("document" in self) { const s = document.createElement("script"); s.src = l, s.onload = e, document.head.appendChild(s) } else s = l, importScripts(l), e() })).then((() => {
+    //     let s = e[l]; if (!s) throw new Error(`Module ${l} didn’t register its module`); return s
+    // })));
+    // self.define = (i, n) => {
+    //     const a = s || ("document" in self ? document.currentScript.src : "") || location.href;
+    //     if (e[a])
+    //         return;
+    //     let t = {};
+    //     const u = s => l(s, a), r = { module: { uri: a }, exports: t, require: u };
+    //     e[a] = Promise.all(i.map((s => r[s] || u(s)))).then((s => (n(...s), t)))
+    // }
 }
 define(["./workbox-7d6a3f4d"], (function (s) {
     navigator.serviceWorker && navigator.serviceWorker.getRegistrations().then(function (registrations) {
